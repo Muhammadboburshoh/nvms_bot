5b565338ddb5746b085365af9674bf1b484946e3
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-01-27 07:46:46 +0500

    initial sourse

---
+++ setup/model.sql
@@ -0,0 +1,26 @@
+create admins(
+    id
+    login
+    password
+)
+
+schools(
+    id
+    name
+    login
+    password
+)
+
+parents(
+    id
+    phone
+    telegram_id
+    password
+)
+
+attendance (
+    id
+    name
+    file
+    school_id
+)
