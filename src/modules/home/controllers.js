669e0d8a789b7632d79e38cc0221a0d68bd53458
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-02-09 19:07:56 +0500

    Add home page

--- src/modules/home/controllers.js
+++ src/modules/home/controllers.js
@@ -9,33 +9,29 @@ const { site_host } = require("../../../config")
 */
 router.get("/", async(req, res) => {
 
+    req.headers.access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IjExOC1tYWt0YWIiLCJsb2dpbiI6IjExOCIsImlhdCI6MTY0NDM3Mjc2NSwiZXhwIjoxNjQ3OTcyNzY1fQ.ujjJ33LRQkFEKEJFg42LRJ9ciEazABAnWyBxLopEBsk"
+
     try {
-        const user = await verify(req.headers.access_token)
-        if(user) {
-            res.render("index", {site_host: site_host})
+        if(req.headers.access_token) {
+            const user = await verify(req.headers.access_token)
+
+            if(user) {
+                res.render("index", {site_host: site_host})
+            }
+            else {
+                res.status(400).end()
+            }
         }
         else {
             res.status(400).end()
         }
+        
     }
     catch(err) {
-        res.statusMessage = err
+        // res.statusMessage = err
         res.status(401).end()
         console.log(err);
     }
-    
-
-    // try {
-
-    //     const user = await verify(req.headers.access_token)
-    //     if(user)
-    //     res.render("index", {site_host: site_host})
-    // }
-    // catch(err) {
-    //     res.statusMessage = err
-    //     res.status(401).end()
-    //     console.log(err);
-    // }
 })
 
 /*
